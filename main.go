2560ed9ae7f9d68fde4ae4216e90c191c2106a5b
Joerg Kuetemeier <joerg@kuetemeier.de>
2019-08-06 20:51:51 +0200

    Add Version and Commit information.

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 // main.go
 package main
 
-var version="unknown"
+var version = "unknown"
+var commitHash = "unknown"
+var gitTag = "unknown"
 
 func main() {
-  println("Ba dum, tss!")
-  println("")
-  println("This is version: " + version)
+	println("Ba dum, tss!")
+	println("")
+	println("This is version: " + version)
+	println("Commit Hash: " + commitHash)
+	println("Git Tag: " + gitTag)
 }
