fa7c767fc9378229fa133b4d640fbefcf1eb9d00
Joerg Kuetemeier <joerg@kuetemeier.de>
2019-08-07 12:55:23 +0200

    Enable Release again.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-go build -i -v -ldflags "-s -w -X github.com/kuetemeier/test/config.version=$(git describe --always --long --dirty)"
+go build -i -v -ldflags "-s -w -X github.com/kuetemeier/test/config.Version=$(git describe --always --long --dirty)"
